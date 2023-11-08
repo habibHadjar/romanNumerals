dce77ea0baff6c57749c73b3d9a46843cc259a43
habibHadjar <hadjar.habib.dev@gmail.com>
2023-11-08 10:42:12 +0100

    added conversion to romanNumerals

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { fileURLToPath } from 'url'
 import { create } from 'express-handlebars'
+import ToRomanNumeral from './romanNumerals.js'
 import { config } from 'dotenv'
 config()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = new URL('.', `file://${__filename}`).pathname;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = new URL('.', `file://${__filename}`).pathname
 
 const PORT = process.env.PORT || 4242
 
@@ -14,11 +15,25 @@ const app = express()
 const hbs = create()
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
-app.set('views', `${__dirname}/views`);
+app.set('views', `${__dirname}/views`)
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('home', {test: 'test'})
+    res.render('home')
 })
+
+app.post('/submit', (req, res) => {
+    let inputValue = req.body.number
+    inputValue = parseInt(inputValue)
+    inputValue = Math.abs(inputValue)
+    inputValue = Math.floor(inputValue)
+    inputValue != NaN ? inputValue : 0
+    
+    const romanNumeral = ToRomanNumeral(inputValue)
+
+    // Render the updated form view with the result
+    res.render('home', { result: romanNumeral })
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
